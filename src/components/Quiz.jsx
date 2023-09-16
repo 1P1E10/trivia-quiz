e6e0f46182b2134ffc4992a7bc52475bec20f304
1P1E10 <98293506+1P1E10@users.noreply.github.com>
2023-09-16 10:06:45 -0400

    Add code to determine if user selected answer is correct

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -10,11 +10,14 @@ function Quiz() {
 
   const [isLoading, setIsLoading] = useState(false);
   const [question, setQuestion] = useState("");
-  const [correctAnswer, setCorrectAnswer] = useState("");
   const [choiceOne, setChoiceOne] = useState("");
   const [choiceTwo, setChoiceTwo] = useState("");
   const [choiceThree, setChoiceThree] = useState("");
   const [choiceFour, setChoiceFour] = useState("");
+  const [choiceOneVal, setChoiceOneVal] = useState(false);
+  const [choiceTwoVal, setChoiceTwoVal] = useState(false);
+  const [choiceThreeVal, setChoiceThreeVal] = useState(false);
+  const [choiceFourVal, setChoiceFourVal] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
@@ -26,30 +29,34 @@ function Quiz() {
           throw new Error("Failed to request quiz question");
         }
 
-        console.log(json);
-
-        setCorrectAnswer(json[0].correctAnswer);
-
         // Randomize the choices' order
         let choices = [
-          json[0].correctAnswer,
-          json[0].incorrectAnswers[0],
-          json[0].incorrectAnswers[1],
-          json[0].incorrectAnswers[2],
+          { choice: json[0].correctAnswer, isCorrect: true },
+          { choice: json[0].incorrectAnswers[0], isCorrect: false },
+          { choice: json[0].incorrectAnswers[1], isCorrect: false },
+          { choice: json[0].incorrectAnswers[2], isCorrect: false },
         ];
         choices.sort(() => Math.random() - 0.5);
         setIsLoading(false);
         setQuestion(json[0].question);
-        setChoiceOne(choices[0]);
-        setChoiceTwo(choices[1]);
-        setChoiceThree(choices[2]);
-        setChoiceFour(choices[3]);
+        setChoiceOne(choices[0].choice);
+        setChoiceTwo(choices[1].choice);
+        setChoiceThree(choices[2].choice);
+        setChoiceFour(choices[3].choice);
+        setChoiceOneVal(choices[0].isCorrect);
+        setChoiceTwoVal(choices[1].isCorrect);
+        setChoiceThreeVal(choices[2].isCorrect);
+        setChoiceFourVal(choices[3].isCorrect);
       } catch (err) {
         console.log(err);
       }
     }
     getQuestion();
   }, []);
+
+  function handleClick(e) {
+    console.log(e.target.value);
+  }
 
   return (
     <div className={styles.page}>
@@ -62,10 +69,34 @@ function Quiz() {
         <>
           <h2 className={styles.Question}>{question}</h2>
           <div className={styles.choices}>
-            <button className={styles.btn1}>{choiceOne}</button>
-            <button className={styles.btn2}>{choiceTwo}</button>
-            <button className={styles.btn3}>{choiceThree}</button>
-            <button className={styles.btn4}>{choiceFour}</button>
+            <button
+              className={styles.btn1}
+              onClick={handleClick}
+              value={choiceOneVal}
+            >
+              {choiceOne}
+            </button>
+            <button
+              className={styles.btn2}
+              onClick={handleClick}
+              value={choiceTwoVal}
+            >
+              {choiceTwo}
+            </button>
+            <button
+              className={styles.btn3}
+              onClick={handleClick}
+              value={choiceThreeVal}
+            >
+              {choiceThree}
+            </button>
+            <button
+              className={styles.btn4}
+              onClick={handleClick}
+              value={choiceFourVal}
+            >
+              {choiceFour}
+            </button>
           </div>
         </>
       )}
